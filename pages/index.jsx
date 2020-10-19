9aee792a806d34f9c1018fc9c777548f7706cd77
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-10-19 15:47:20 -0500

    completed course

---
+++ pages/index.jsx
@@ -0,0 +1,33 @@
+// pages/index.jsx
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import Link from "next/link";
+
+export default ({ content }) => (
+  <div sx={{ height: `calc(100vh - 60px)` }}>
+    <div
+      sx={{
+        variant: "containers.page",
+        display: "flex",
+        alignItems: "center",
+        height: "100%",
+      }}
+    >
+      <h1 sx={{ fontSize: 8, my: 0 }}>
+        {content.title}
+      </h1>
+    </div>
+  </div>
+);
+
+export function getStaticProps() {
+  // get data from CMS
+
+  return {
+    props: {
+      content: {
+        title: 'This is my really nice app'
+      }
+    }
+  }
+}
